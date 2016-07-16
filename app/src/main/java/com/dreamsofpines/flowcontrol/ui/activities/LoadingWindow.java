c0b33e587a30868bd913b1f75721ee6147cfabe9
Pupsik <ThePupsick@yandex.ru>
2016-07-16 19:21:23 +0700

    In loading menu check the first open app

--- app/src/main/java/com/dreamsofpines/flowcontrol/ui/activities/LoadingWindow.java
+++ app/src/main/java/com/dreamsofpines/flowcontrol/ui/activities/LoadingWindow.java
@@ -1,16 +1,43 @@
 package com.dreamsofpines.flowcontrol.ui.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.dreamsofpines.flowcontrol.R;
 
 /**
  * Created by ThePupsick on 16.07.16.
  */
 public class LoadingWindow extends AppCompatActivity {
 
+    private static final String MY_SETTINGS = "my_settings";
+    private SharedPreferences mySetting;
+    private SharedPreferences.Editor edit;
+
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.loadingwindow);
+
+        // Create settings file
+         mySetting = getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE);
+
+        // Check the first open
+        boolean hasVisited = mySetting.getBoolean("hasVisited", false);
+        if(!hasVisited){
+
+            // insert layout with registration !!!
+            edit = mySetting.edit();
+            edit.putBoolean("hasVisited",true);
+            edit.commit();
+
+        } else {
+            Intent intent = new Intent(LoadingWindow.this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
